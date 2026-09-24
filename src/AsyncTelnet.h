9cab3316da5298286fdf58f36d07bef520d10db6
Frank <FrankBoesing@users.noreply.github.com>
2026-09-24 14:04:04 +0200

    Refactor AsyncTelnet class structure and members
    Removed unused includes and mutex protection, added IP address member.

--- src/AsyncTelnet.h
+++ src/AsyncTelnet.h
@@ -9,17 +9,12 @@
 #error Platform not supported
 #endif
 
-#include <functional>
-#include <mutex>
-
 #ifndef HANDLE_INCOMMING_DATA
 #define HANDLE_INCOMMING_DATA false
 #endif
 
 #if HANDLE_INCOMMING_DATA
 
-// Annahme: 256 Zeichen sind für Telnet-Kommandos ausreichend.
-// Kann bei Bedarf vor dem Include überschrieben werden.
 #ifndef ASYNC_TELNET_BUFFER_SIZE
 #define ASYNC_TELNET_BUFFER_SIZE 256
 #endif
@@ -33,6 +28,7 @@ typedef std::function<void(const char *)> IncomingDataHandler;
 class AsyncTelnet
 {
 public:
+
     explicit AsyncTelnet(uint16_t port = 23)
         : server(port),
           client(nullptr),
@@ -42,11 +38,8 @@ public:
 
     ~AsyncTelnet();
 
-    // AsyncTelnet ist nicht kopierbar oder verschiebbar.
     AsyncTelnet(const AsyncTelnet &) = delete;
     AsyncTelnet &operator=(const AsyncTelnet &) = delete;
-    AsyncTelnet(AsyncTelnet &&) = delete;
-    AsyncTelnet &operator=(AsyncTelnet &&) = delete;
 
     bool begin(bool checkConnection = false);
     void close();
@@ -59,7 +52,10 @@ public:
     bool connected();
     void disconnectClient();
 
-    IPAddress getLastAttemptIP() const;
+    IPAddress getLastAttemptIP() const
+    {
+        return ip;
+    }
 
     void onConnect(ConnHandler callbackFunc);
     void onDisconnect(DisconnHandler callbackFunc);
@@ -69,8 +65,10 @@ public:
 #endif
 
 protected:
+
     AsyncServer server;
     AsyncClient *client;
+
     IPAddress ip;
     uint16_t server_port;
 
@@ -78,17 +76,13 @@ protected:
     DisconnHandler on_disconnect = nullptr;
 
 #if HANDLE_INCOMMING_DATA
+
     IncomingDataHandler on_incoming_data = nullptr;
 
     char buffer[ASYNC_TELNET_BUFFER_SIZE] = {};
     size_t buf_ptr = 0;
-#endif
 
-    // Schützt Client-Pointer und Callback-Objekte.
-    // Wichtig: Der Mutex wird niemals während c->close()
-    // gehalten, da close() synchron den Disconnect-Callback
-    // auslösen kann.
-    mutable std::mutex mutex;
+#endif
 };
 
 #endif
